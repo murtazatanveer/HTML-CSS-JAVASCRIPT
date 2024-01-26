a2a92e9064818ef3524b25b627ee7e1a9452e30b
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-01-27 01:05:43 +0500

    my first code in JS.

--- first_program.js
+++ first_program.js
@@ -1,11 +1,29 @@
-console.log("WELCOME TO JAVASCRIPT");
-console.log("Murtaza Tanveer");
-console.log("My First Code in JS");
+// console.log("WELCOME TO JAVASCRIPT");
+// console.log("MY Name is Murtaza Tanveer");
+// console.log("My First Code in JS.....");
+
+
+{
+  full_name = "murtaza tanveer";
+   age =20;
+  address = "jhelum";
+}
+
+{
+    console.log("My Introduction\n","Name : ",full_name , "\n","Age : ",age,"\n","Address : ",address);
+}
+
+
+
 
 
 // let a = 10;
-// let b= 10;
-//  let c= a+b;
+// let b= 5;
+
+// console.log("Sum : ",a+b);
+// console.log("Multiply : ",a*b);
+// console.log("Sub : ",a-b);
+// console.log("Sum : ",a/b);
 
 //  if(c==20){
 
