65589630a0dffaa5d86bc6bceaa21ee33affce96
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-01-26 23:36:13 +0500

    first_program.js

---
+++ first_program.js
@@ -0,0 +1,14 @@
+console.log("WELCOME TO JAVASCRIPT");
+console.log("Murtaza Tanveer");
+console.log("My First Code in JS");
+
+
+// let a = 10;
+// let b= 10;
+//  let c= a+b;
+
+//  if(c==20){
+
+// console.log(a+b);
+
+//  }
