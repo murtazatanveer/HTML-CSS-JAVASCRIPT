1a01d8f2dc937b85153374c84f86918878789b08
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-01-31 13:17:32 +0500

    objects in js

---
+++ Variables and Datatypes/first_program.js
@@ -0,0 +1,184 @@
+// /* Demo Code of Variables and Datatypes is JS */
+
+
+// console.log("WELCOME TO JAVASCRIPT");
+// console.log("MY Name is Murtaza Tanveer");
+// console.log("My First Code in JS. (24th Jan 2023)");
+
+// let full_name = "murtaza tanveer";
+
+// {
+  
+//     age=21;
+//   address = "jhelum";
+//  // a = address + age;
+
+// }
+// //console.log("\n",a,"\n");
+// {
+   
+
+//     console.log("My Introduction\n","Name : ",full_name , "\n","Age : ",age,"\n","Address : ",address);
+// }
+
+
+
+// let a = 10;
+// let b= 5;
+
+// console.log("Sum : ",a+b);
+// console.log("Multiply : ",a*b);
+// console.log("Sub : ",a-b);
+// console.log("Sum : ",a/b);
+
+//  if((a+b)==15){
+
+// console.log(a+b);
+
+//  }
+
+
+// let str = "murtaza ";
+// let str_2 = "tanveer";
+// let number = 15;
+// let num_2 = 5;
+
+// console.log(str+str_2);
+
+// if(isNaN(number*num_2)){
+
+//     console.log("FINE");
+//      console.log(number*num_2);
+//      console.log(isNaN(number*num_2));
+
+// }
+
+// else{
+//     console.log("NOT FINE");
+//      console.log(number*num_2);
+// }
+
+// let variable_1;
+// console.log(variable_1);
+
+//  console.log("str + number = ",str+number);
+
+//  console.log("str - number = ",str-number);
+
+
+// let variable;
+// console.log(variable); // Outputs: undefined
+
+// function exampleFunction(param) {
+//   console.log(param); // Outputs: undefined if the function is called without an argument
+// }
+
+// exampleFunction();
+
+// let price =100;
+
+// console.log(typeof(price));
+
+// let uniqueKey = Symbol("unique");
+
+// console.log(uniqueKey);
+
+// name_1 = "murtaza";
+
+
+
+
+// //Objects in JS
+
+// let person= new Object();
+
+//  person.name="murtaza";
+
+//  person.age = 21;
+
+//  person.address="Jhelum";
+
+//  console.log(person);
+
+//  { person = {
+
+//  name : "murtaza",
+//  age:21,
+// address:"Jhelum"
+
+// };
+
+// person.is_student=false;
+
+// }
+
+// console.log(person);
+
+// const person_2 = {};
+
+// person_2.name = "moon";
+
+// person_2.address = "p.d.khan";
+
+// person_2.is_student = false;
+
+// console.log(person_2);
+
+
+//  console.log(" Name : ",person.name,"\n","Age : ",person.age,"\n","Address",person.address);
+
+//  x = person;
+
+// x.age=35;
+
+// console.log("\n\n Name : ",person.name,"\n","Age : ",person.age,"\n","Address",person.address);
+
+//  const person = {
+
+//  name : "moon",
+// age:21,
+// address:"J",
+// is_student:true
+
+// }
+
+// person.address="kot phaphra";
+
+// person.gender="male";``
+
+// console.log(person);
+
+// alert("Murtaza here");
+
+// {
+//     let str="10";
+//     let num = "5";
+//     let result = num * str;
+//     console.log(typeof(result),result);
+// }
+
+// {
+
+// const vehicle = {
+
+//     compay : "Honda",
+//     type : "Bike",
+//     colour : "Black",
+//     cc : "CD70",
+//     owner : {
+
+//         name : "murtaza",
+//         address : "jhelum",
+//     }
+
+// }
+
+// vehicle.owner.age=21;
+
+
+// console.log(vehicle.owner.address);
+
+// console.log(vehicle);
+
+//}
+
