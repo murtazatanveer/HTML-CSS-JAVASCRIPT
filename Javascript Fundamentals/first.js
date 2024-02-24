aec4575ce3f884acd946547f16de2baa3972d70a
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-02-24 18:23:45 +0500

    Function to calculate the age of a person

--- Javascript Fundamentals/first.js
+++ Javascript Fundamentals/first.js
@@ -1,326 +1,409 @@
 
 
-// // Number guessing game. This Game generates a random number between 1 and 500 and you want to guess it.
+ // // Number guessing game. This Game generates a random number between 1 and 500 and you want to guess it.
 
- //number_guess();
+//  //number_guess();
 
- function number_guess(){
+//  function number_guess(){
 
-let num = Math.floor(Math.random() * 500) + 1;
+// let num = Math.floor(Math.random() * 500) + 1;
 
-let guess = 0;
+// let guess = 0;
 
-let guess_counter = 0;
+// let guess_counter = 0;
 
-    while(1){
+//     while(1){
 
-guess = prompt("Enter a Number : ");
+// guess = prompt("Enter a Number : ");
 
-guess_counter++;
+// guess_counter++;
 
-if(num==guess){
+// if(num==guess){
 
-alert("CONGRATULATIONS! You Guess correctly the original number is "+num+"\nYour total guesses are "+guess_counter);
+// alert("CONGRATULATIONS! You Guess correctly the original number is "+num+"\nYour total guesses are "+guess_counter);
 
-break;
+// break;
+
+// }
+
+// else{
+
+// if(guess>num){alert("Your Guess is greator than number");}
+
+// else{alert("Your Guess is Smaller than number");}
+
+// }
+
+
+//  }
+
+    
+//  }
+
+
+//  /* Steven wants to build a very simple tip calculator for whenever he goes eating in a 
+//  restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 
+//  300. If the value is different, the tip is 20% */
+
+
+// let total = 0;
+
+// function tip_calculator(bill){
+
+// let tip = bill >=50 && bill <=300 ? (15*bill)/100 : (20*bill)/100;
+
+// total = tip + bill;
+
+// return tip;
+
+// }
+
+// bill = 30;
+
+// //console.log("The bill was",bill,",the tip was",tip_calculator(bill),", and the total value",total);
+
+
+// { //Template Strings
+
+
+// const birth_year = 2003;
+
+// let current_year = 2024;
+
+// const name = "Murtaza Tanveer"
+
+// const job = "Software Engineer";
+
+//     let me = `Hi I am ${name}, a ${current_year - birth_year} years old ${job}.`;
+
+//     console.log(me);
+    
+// }
+
+//  // Type Conversion
+
+// {
+
+// let num_1 = 10;
+
+// let num_2 = "20";
+
+// let concatinate = num_1 + num_2;
+
+// let sum = num_1 +  Number(num_2);
+
+// console.log(`Concatinate : ${concatinate}\nSum : ${sum}`);
+
+// let n1 = 10;
+
+// let n2 = 5;
+
+// console.log(n1 + String(n2));
+
+// let x = 5 + "5" - 55;
+
+// console.log(x,typeof(x));
+
+// }
+
+//  // Truthy and Falsy Values
+
+// {
+//     // 5 Falsy values are 0 , undefined , null , "" (empty string) and NaN
+
+//     console.log(Boolean(0));
+//     console.log(Boolean(undefined));
+//     console.log(Boolean(NaN));
+//     console.log(Boolean(null));
+//     console.log(Boolean(""));
+
+// }
+
+ 
+
+
+// // Function to find Reverse of a number.
+
+// console.log(`Reverse of 1234 is ${Reverse(1234)}.`);
+
+// function Reverse(num)
+// {
+//     let digit = 0;
+//     let rev = 0;
+
+//     while(num!=0){
+
+//         digit = num % 10;
+//         rev = (rev * 10) + digit;
+//         num = Math.floor(num/10);
+    
+//     }
+
+// return rev;
+
+// }
+
+
+
+// //Function that returns the nth Fab number.
+
+//  function fab(n) {
+
+
+//     let f0 = 0,f1 = 1,current_f = 0;
+
+//     for(let i = 2;i<=n;i++){
+
+//         current_f = f0 + f1;
+
+//         f0=f1;
+
+//         f1=current_f;
+        
+
+//     }
+    
+//     return current_f;
+
+// };
+
+
+// // Ternary Operator ?
+// {
+
+//     let age = 21;
+//     let vote = age>=18 ? "You have right to vote" : "You donot have right to vote";
+//     console.log(vote);
+
+// }
+
+
+
+// // For loops
+
+//  {
+
+
+   
+//     const person = {
+
+//         name: 'xyz',
+//         age: 21,
+//         city: 'lahore',
+//         Hobby: "Programmer"
+
+//       };
+
+      
+      
+
+
+//       for(let key in person){
+
+//         console.log(`${key} : ${person[key]}`);
+        
+//       }
+
+
+
+//       const bike = {
+
+//         company : "Honda",
+        
+//         cc : 70,
+
+//         owner : {
+
+//             name : "moon",
+//              age : 21,
+
+//         },
+
+//         colour : "Black",
+
+       
+
+
+//       };
+      
+      
+
+//       let k;
+
+//       let i = 1;
+
+//       for(k in bike){
+
+//         if(i==3){
+
+//             let inner_key
+        
+//             console.log(`${k} :\n`);
+
+//             for(inner_key in (bike.owner)){
+
+                
+//                 console.log(`     ${inner_key} : ${bike.owner[inner_key]}`);
+
+
+//             }
+
+
+
+//         }
+
+//         else{
+
+//         console.log(`${k} : ${bike[k]}`);}
+
+//         i++;
+
+        
+//       }
+      
+
+//       let numbers = [10,20,30,40];
+
+
+//       for(let index in numbers){
+
+//         console.log(`${index} : ${numbers[index]}`);
+
+
+//       }
+
+//     const university = {
+
+//         name : "Comsats",
+//         branch : "Abbottabad",
+//         is_private : true,
+//     }
+
+//     const array = Object.keys(university);
+
+//     for(let key of array){
+
+//         console.log(key);
+//     }
+
+//     const arr = Object.values(university);
+
+   
+
+//     for(let key in arr){
+
+
+//         console.log(key+" : ",arr[key]);
+//     }
+
+    
+
+
+// }
+
+/* Write a program to print the marks of student in an object "marks" using for loop*/
+
+//{
+    // const marks = {
+
+    //     stu_1 : 90,
+    //     stu_2 : 80,
+    //     stu_3 : 75,
+    //     stu_4 : 50,
+    // }
+
+    
+
+    // for(let i=0; i<= (Object.keys(marks).length-1); i++){
+
+    //     console.log(`Marks of ${Object.keys(marks)[i]} is ${Object.values(marks)[i]}`);
+
+    // }
+
+    // /* Same problem using "for in" loop */
+
+    // for(let key in marks){
+
+    //     console.log(`Marks of ${key} is ${marks[key]}`);
+    // }
+
+
+//}
+
+/* Function to calculate age of a person */
+
+calculate_age();
+
+function calculate_age(){
+
+const birth_year = Number(prompt("Enter the Year in which you were born : "));
+
+const birth_month = Number(prompt("Enter the Month in which you were born : "));
+
+const birth_date = Number(prompt("Enter the Date in which you were born : "));
+
+const today = new Date();
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1; 
+const date = today.getDate();
+
+let age_month = 0, age_year = 0, age_Date = 0
+
+if(month==birth_month && date==birth_date){
+
+    age_year = year - birth_year;
+
+}
+
+else if(birth_month > month || ((birth_month==month)&&(birth_date>date))){
+
+    age_year = (year - birth_year)-1;
+
+if(month<birth_month){
+    age_month = (12-birth_month) + month;
+    age_Date = (30.42-birth_date) - (30.42-date);
+
+}
+
+    else {
+
+        age_month = ((12-birth_month) + month) - 1;
+        age_Date = birth_date-date;
+    }   
+}
+
+else{
+
+    age_year = (year - birth_year);
+
+    if(month==birth_month){
+
+    age_month = 0;
+    age_Date = date - birth_date;
 
 }
 
 else{
 
-if(guess>num){alert("Your Guess is greator than number");}
+    age_month =  (month-birth_month);
 
-else{alert("Your Guess is Smaller than number");}
+    if(date>birth_date){
 
-}
-
-
- }
-
-    
- }
-
-
- /* Steven wants to build a very simple tip calculator for whenever he goes eating in a 
- restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 
- 300. If the value is different, the tip is 20% */
-
-
-let total = 0;
-
-function tip_calculator(bill){
-
-let tip = bill >=50 && bill <=300 ? (15*bill)/100 : (20*bill)/100;
-
-total = tip + bill;
-
-return tip;
-
-}
-
-bill = 30;
-
-//console.log("The bill was",bill,",the tip was",tip_calculator(bill),", and the total value",total);
-
-
-{ //Template Strings
-
-
-const birth_year = 2003;
-
-let current_year = 2024;
-
-const name = "Murtaza Tanveer"
-
-const job = "Software Engineer";
-
-    let me = `Hi I am ${name}, a ${current_year - birth_year} years old ${job}.`;
-
-    console.log(me);
-    
-}
-
- // Type Conversion
-
-{
-
-let num_1 = 10;
-
-let num_2 = "20";
-
-let concatinate = num_1 + num_2;
-
-let sum = num_1 +  Number(num_2);
-
-console.log(`Concatinate : ${concatinate}\nSum : ${sum}`);
-
-let n1 = 10;
-
-let n2 = 5;
-
-console.log(n1 + String(n2));
-
-let x = 5 + "5" - 55;
-
-console.log(x,typeof(x));
-
-}
-
- // Truthy and Falsy Values
-
-{
-    // 5 Falsy values are 0 , undefined , null , "" (empty string) and NaN
-
-    console.log(Boolean(0));
-    console.log(Boolean(undefined));
-    console.log(Boolean(NaN));
-    console.log(Boolean(null));
-    console.log(Boolean(""));
-
-}
-
- 
-
-
-// Function to find Reverse of a number.
-
-console.log(`Reverse of 1234 is ${Reverse(1234)}.`);
-
-function Reverse(num)
-{
-    let digit = 0;
-    let rev = 0;
-
-    while(num!=0){
-
-        digit = num % 10;
-        rev = (rev * 10) + digit;
-        num = Math.floor(num/10);
-    
-    }
-
-return rev;
-
-}
-
-
-
-//Function that returns the nth Fab number.
-
- function fab(n) {
-
-
-    let f0 = 0,f1 = 1,current_f = 0;
-
-    for(let i = 2;i<=n;i++){
-
-        current_f = f0 + f1;
-
-        f0=f1;
-
-        f1=current_f;
+        age_Date = date - birth_date;
         
+    }
+
+    else{
+
+        age_month--;
+        age_Date = 30.42-(birth_date-date);
 
     }
+}
     
-    return current_f;
-
-};
-
-
-// Ternary Operator ?
-{
-
-    let age = 21;
-    let vote = age>=18 ? "You have right to vote" : "You donot have right to vote";
-    console.log(vote);
-
 }
 
-
-
-// For loops
-
- {
-
-
-   
-    const person = {
-
-        name: 'xyz',
-        age: 21,
-        city: 'lahore',
-        Hobby: "Programmer"
-
-      };
-
-      
-      
-
-
-      for(let key in person){
-
-        console.log(`${key} : ${person[key]}`);
-        
-      }
-
-
-
-      const bike = {
-
-        company : "Honda",
-        
-        cc : 70,
-
-        owner : {
-
-            name : "moon",
-             age : 21,
-
-        },
-
-        colour : "Black",
-
-       
-
-
-      };
-      
-      
-
-      let k;
-
-      let i = 1;
-
-      for(k in bike){
-
-        if(i==3){
-
-            let inner_key
-        
-            console.log(`${k} :\n`);
-
-            for(inner_key in (bike.owner)){
-
-                
-                console.log(`     ${inner_key} : ${bike.owner[inner_key]}`);
-
-
-            }
-
-
-
-        }
-
-        else{
-
-        console.log(`${k} : ${bike[k]}`);}
-
-        i++;
-
-        
-      }
-      
-
-      let numbers = [10,20,30,40];
-
-
-      for(let index in numbers){
-
-        console.log(`${index} : ${numbers[index]}`);
-
-
-      }
-
-    const university = {
-
-        name : "Comsats",
-        branch : "Abbottabad",
-        is_private : true,
-    }
-
-    const array = Object.keys(university);
-
-    for(let key of array){
-
-        console.log(key);
-    }
-
-    const arr = Object.values(university);
-
-   
-
-    for(let key in arr){
-
-
-        console.log(key+" : ",arr[key]);
-    }
-
-    
-
-
-}
-
-/* Write a program to print the marks of student in an object "marks" using for loop*/
-
-{
-    const marks = {
-
-        stu_1 : 90,
-        stu_2 : 80,
-        stu_3 : 75,
-        stu_4 : 50,
-    }
-
-    
-
-    for(let i=0; i<= (Object.keys(marks).length-1); i++){
-
-        console.log(`Marks of ${Object.keys(marks)[i]} is ${Object.values(marks)[i]}`);
-
-    }
-
-
-
+alert(`You are ${age_year} years , ${age_month} months and ${age_Date} days old`);
 
 }
