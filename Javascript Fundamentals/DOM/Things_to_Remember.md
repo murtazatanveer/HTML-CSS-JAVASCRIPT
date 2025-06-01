bf532735072a9f21fa97c96b9f2c8cc202a2c5ed
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2025-06-01 21:37:27 +0500

    Creating , inserting new HTML Elements and Removing and Styling HTML Elements of DOM Tree.

--- Javascript Fundamentals/DOM/Things_to_Remember.md
+++ Javascript Fundamentals/DOM/Things_to_Remember.md
@@ -1,5 +1,13 @@
 => Understanding what is DOM
 
+=> Dynamic behaviour of DOM
+
 => Accessing HTML elements (Id, ClassName, TagName, querySelector , querySelectorAll).
 
-=> Updating HTML elements (textcontent, innerText, )
+=> Updating HTML elements (textcontent, innerText, innerHTML , outerHTML)
+
+=> Creating and inserting new HTML Elements (creteElement, appendChild, pretend, insertAdjacentElement, insertAdjacentHTML, insertBefore,before,after, clonenode(true and false), copyingALLchild elements, parentElement)
+
+=>Removing HTML Elements( element.remove(),parentElement.removeChild(childElement))
+
+=> Styling HTML Elements (element.style, cssText, setAttribute)
--- Javascript Fundamentals/DOM/dom.js
+++ Javascript Fundamentals/DOM/dom.js
@@ -8,25 +8,110 @@ console.log(allInnerBoxes);
 
 // allBoxes[2].innerHTML = "HIII";
 
-const box = document.getElementsByClassName("box");
+const box = document.getElementsByClassName("box"); // section tag
 console.log(box[0]);
 
 const alLText = document.querySelectorAll("p"); // Returns an array
 console.log(alLText);
 
-b1.innerHTML = '<Button id="b">CLICK ME</Button>'; // Overrides <p></p> in b1
-console.log(b1.innerHTML);
+// Updating HTML ElementsUpdating HTML elements (textcontent, innerText, innerHTML , outerHTML)
+// b1.innerHTML = '<Button id="b">CLICK ME</Button>'; // Overrides <p></p> in b1
+// console.log(b1.innerHTML);
 
-b1.innerHTML = '<Button id="but">CLICK</Button>'; // Overrides existing Button with id "b"
-console.log(b1.innerHTML);
+// b1.innerHTML = '<Button id="but">CLICK</Button>'; // Overrides existing Button with id "b"
+// console.log(b1.innerHTML);
 
-b1.outerHTML = '<h1 id="dom">DOM</h1>'; //This is the key point: const b1 still holds a reference to the original div element that was removed from the DOM.
-console.log(b1.innerHTML);
+// b1.outerHTML = '<h1 id="dom">DOM</h1>'; //This is the key point: const b1 still holds a reference to the original div element that was removed from the DOM.
+// console.log(b1.innerHTML);
 
-const newElement = document.getElementById("dom");
+// const newElement = document.getElementById("dom");
 
-console.log(b1.outerHTML + "\t" + newElement.outerHTML);
+// console.log(b1.outerHTML + "\t" + newElement.outerHTML);
 
-console.log(box[0].innerText);
+// console.log(box[0].innerText);
 
-console.log(box[0].textContent);
+// console.log(box[0].textContent);
+
+// Creating new HTML Elements
+
+const lastDiv = document.createElement("div");
+
+console.log(lastDiv);
+
+lastDiv.id = "last";
+
+lastDiv.innerHTML = "<p>Last Box</p>";
+
+lastDiv.style.background = "#2b2222";
+
+box[0].appendChild(lastDiv);
+
+const firstDiv = document.createElement("div");
+
+firstDiv.innerHTML = "<p>First Box</p>";
+firstDiv.id = "first";
+firstDiv.style.background = "orange";
+
+box[0].insertAdjacentElement("afterbegin", firstDiv);
+
+// box[0].insertAdjacentHTML(
+//   "afterbegin",
+//   '<div id="first"><p>First Box</p></div>'
+// );
+
+const middleBox = document.createElement("div");
+
+middleBox.id = "middle";
+middleBox.innerHTML = "<p>Middle Box</p>";
+middleBox.style.background = "#9fff";
+middleBox.style.color = "black";
+middleBox.style.fontWeight = "bolder";
+
+box[0].insertBefore(middleBox, allInnerBoxes[4]);
+
+const lb = document.getElementById("lowerBox");
+
+const div1 = b1.cloneNode(true);
+
+lb.prepend(div1);
+
+const div6 = allInnerBoxes[7].cloneNode(true);
+div1.after(allInnerBoxes[2].cloneNode(true), div6);
+
+div6.before(allInnerBoxes[6].cloneNode(true), allInnerBoxes[5].cloneNode(true));
+
+// const copyInnerBoxes = Array.from(box[0].children);
+
+const box1Parent = b1.parentElement;
+
+console.log(box1Parent);
+
+//box1Parent.style.display = "none";
+
+//console.log((box1Parent.style.cssText = "display:none"));
+
+// lb.style.flexDirection = "column";
+// lb.style.width = "50%";
+// lb.style.height = "70%";
+
+// lb.style.cssText =
+//   "background:transparent; flex-direction:column; width:30%; height:70%;";
+
+// box[0].remove();
+
+// const lbChilds = lb.querySelectorAll("*");
+
+// lbChilds=lb.children;
+
+// console.log(lbChilds);
+
+// lbChilds[0].innerHTML = "<h4>HELLO G</h4>";
+
+// b1.setAttribute("style", "display:none");
+
+// b1.setAttribute("class", "box1");
+// b1.setAttribute("id", "ub1");
+
+// const p = b1.getElementsByTagName("p");
+
+// p[0].innerHTML = "<h1>HELLO</h1>";
